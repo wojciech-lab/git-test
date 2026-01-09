d709cde707241e472d0947d9761d493e5e5b542f
Wojciech-Lab <Kantorwojciech2002@gmail.com>
2026-01-09 17:44:37 +0100

    Initial commit

---
+++ Chapter1.py
@@ -0,0 +1,13 @@
+
+#Git init jest to inicjalizacja repozytorium
+
+x = 10
+
+#Git status to sprawdzenie statusu repozytorium
+
+#git add nazwa pliku
+
+# .gitignore - plik do ignorowania plików i ścieżek
+
+
+#git add .  #dodaj wszystko naraz
